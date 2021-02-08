e9d65a5da1365d61c104798ea8dc81cf48b52eca
yamzal1 <69195301+yamzal1@users.noreply.github.com>
2021-02-08 17:58:48 +0100

    debut mouvement camera + zones

--- New Unity Project/Assets/pol.cs
+++ New Unity Project/Assets/pol.cs
@@ -19,17 +19,22 @@ public class pol : MonoBehaviour
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-        //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis("Horizontal");
 
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Vertical");
+        bool isShiftKeyDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        if (!isShiftKeyDown)
+        {
+            //Store the current horizontal input in the float moveHorizontal.
+            float moveHorizontal = Input.GetAxis("Horizontal");
 
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce(movement * speed);
-    }
+            //Store the current vertical input in the float moveVertical.
+            float moveVertical = Input.GetAxis("Vertical");
+
+            //Use the two store floats to create a new Vector2 variable movement.
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+            //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+            rb2d.AddForce(movement * speed);
+        }
+        }
 }
-
---
+++ New Unity Project/Assets/scriptCam.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //GameObject.Find("Player").transform.position;
+
+        bool isShiftKeyDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+        if (isShiftKeyDown)
+        {
+
+            float xAxisValue = Input.GetAxis("Horizontal");
+            float zAxisValue = Input.GetAxis("Vertical");
+            if (Camera.current != null)
+            {
+                Camera.current.transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue));
+            }
+        }
+
+    }
+}
