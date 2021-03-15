5ca0982babd95976a9a8746279219d1536563f1c
JeremyPoisson <76879931+JeremyPoisson@users.noreply.github.com>
2021-03-15 15:17:58 +0100

    Dernière version démo -- V1

---
+++ Scripts/ReglageVolume.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class ReglageVolume : MonoBehaviour
+{
+    [SerializeField] string _parametreVolume = "MasterVolume";
+    [SerializeField] AudioMixer _mixer;
+    [SerializeField] Slider _slider;
+
+    private void Awake()
+    {
+        _slider.onValueChanged.AddListener(modifierValeurMixer);
+    }
+
+    private void modifierValeurMixer(float x)
+    {
+        _mixer.SetFloat(_parametreVolume, x);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
